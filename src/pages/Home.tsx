e4729873c6ea672e98cc0b82eb54ee6b5b5457b0
André Navarro <andre_farao123@hotmail.com>
2021-08-24 20:26:37 -0300

    Adding basic functionalities to todo list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks(oldTasks => [...oldTasks, {done: false, title: newTaskTitle, id: new Date().getTime()}])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const findTaskIndex = tasks.findIndex(task => task.id === id);
+
+    if (findTaskIndex !== -1) {
+      setTasks(oldTasks => {
+        return oldTasks.map(task => {
+          if (task.id === id ) {
+            return {...task, done: !task.done}
+          }
+
+          return task
+        })
+      })
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => {
+      return oldTasks.filter(task => task.id !== id);
+    })
   }
 
   return (
